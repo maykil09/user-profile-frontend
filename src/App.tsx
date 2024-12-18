6bdfbcb5631c0aed2d88381b6b3c0b3ba5335cea
Michael Guilaran <maykil093@gmail.com>
2024-12-18 22:39:18 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+    return (
+        <>
+            <p>User Profile exam</p>
+            <Button>Click me!</Button>
+        </>
+    );
+}
+
+export default App;
